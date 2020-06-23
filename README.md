2f3dda3b75c53ce30bc5471c6d52db31868c4050
Raj <rajsriv.14@gmail.com>
2020-06-23 16:27:34 +0530

    Added login and sessions

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ To Run:
 
 1-> pip install -r requirements.txt
 
-2-> flask run
+2-> python app.py
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Blogs
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -18,28 +19,27 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #Initialize MySQL
 mysql = MySQL(app)
 
+#Index
 @app.route('/')
 def index():
     return render_template('home.html')
 
+#about
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-
+#all Blogs
 @app.route('/blogs')
 def blogs():
     return render_template('blogs.html',blogs = BLOG_DATA)
 
+#Single Blog
 @app.route('/blog/<string:id>')
 def singleBlog(id):
     return render_template('single_blog.html',id=id)
 
+#Register form class
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=5, max=20),validators.DataRequired(),])
@@ -53,6 +53,7 @@ class RegisterForm(Form):
         validators.DataRequired(),
         validators.EqualTo('password', message= 'Passwords do not match')])
 
+#Signup
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm(request.form)
@@ -80,5 +81,68 @@ def signup():
         
     return render_template('signup.html', form=form)
 
-app.secret_key = 'efew_5%&^8ndF4Q8z\c]/'
-app.run(debug=True)
+#Login
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        #get form fields
+        username = request.form['username']
+        password_candidate = request.form['password'] 
+
+        #cursor
+        curr = mysql.connection.cursor()
+        #get user by username
+        result = curr.execute("SELECT * FROM users WHERE username = %s",[username])
+        app.logger.info(result)
+        #if any row found
+        if result > 0:
+            #get hash
+            data = curr.fetchone()
+            password = data['password']
+            #compare pass
+            if sha256_crypt.verify(password_candidate,password):
+                app.logger.info('PASSWORD MATCHED')
+                session['logged_in'] = True
+                session['username'] = username
+                flash('Logged in successfully', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                app.logger.info('INavlid Username/Password')
+                flash('Invalid Username / Password','danger')
+            #Close connection
+            curr.close()
+        else:
+            app.logger.info('NO USER FOUND')
+            flash('Invalid Username / Password','danger')
+    
+    return render_template('login.html')
+
+#check user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorised, Please Login','danger')
+            return redirect(url_for('login'))
+    return wrap
+
+#Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('Logged out successfully !','success')
+    return redirect(url_for('login'))
+
+
+#Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.secret_key = 'efew_5%&^8ndF4Q8z\c]/'
+    app.run(debug=True)
